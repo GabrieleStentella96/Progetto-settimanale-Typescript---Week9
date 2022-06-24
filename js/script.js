d77ab9c0ed63b51cf9e59f8e4ebcf0f33c162c77
GabrieleStentella96 <104636389+GabrieleStentella96@users.noreply.github.com>
2022-06-24 18:09:46 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+class Smartphone {
+}
+class User1 extends Smartphone {
+}
+class User2 extends Smartphone {
+}
+class User3 extends Smartphone {
+}
---
+++ ts/script.ts
@@ -0,0 +1,22 @@
+class Smartphone {
+    private _carica: number;
+    private _numeroChiamate: number; 
+
+    constructor(carica: number, numeroChiamate: number){
+       this._carica = carica;
+       this._numeroChiamate = numeroChiamate;
+    }
+
+    
+}
+
+
+let User1 = new Smartphone(4, 2);
+
+let User2 = new Smartphone(1, 1);
+
+let User3 = new Smartphone(2, 10); 
+
+
+
+
