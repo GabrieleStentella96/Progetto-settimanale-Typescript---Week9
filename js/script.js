9b9be812c1561fe3d220a22dcfd6514a4aecba3a
GabrieleStentella96 <104636389+GabrieleStentella96@users.noreply.github.com>
2022-06-26 21:42:11 +0200

    secondo commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,98 @@
 "use strict";
+const COSTO = 0.20;
 class Smartphone {
+    constructor(charge, numCalls, hours, minutes, seconds, interval) {
+        this.hours = 0;
+        this.minutes = 0;
+        this.seconds = 0;
+        this.charge = charge;
+        this.numCalls = numCalls;
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+        this.interval = interval;
+    }
 }
-class User1 extends Smartphone {
+class User extends Smartphone {
+    constructor(charge, numCalls, hours, minutes, seconds, interval) {
+        super(charge, numCalls, hours, minutes, seconds, interval);
+    }
+    chargeCredit() {
+        if (this.charge <= COSTO) {
+            this.charge = this.charge + 10;
+        }
+        else if (this.charge > COSTO) {
+            this.charge = this.charge + 5;
+        }
+        console.log(this.charge);
+    }
+    smartphoneCall() {
+        this.seconds++;
+        if (this.seconds >= 60) {
+            this.seconds = 0;
+            this.minutes++;
+            this.charge = this.charge - COSTO;
+        }
+        console.log(this.charge);
+    }
+    number404() {
+        let n404 = this.charge - ((this.minutes + 1) * COSTO) - 1;
+        return n404;
+    }
+    callNumber() {
+        if (this.charge >= COSTO) {
+        }
+        console.log(this.numCalls);
+    }
+    eraseCalls() {
+        this.numCalls = this.numCalls * 0;
+        console.log(this.numCalls);
+    }
 }
-class User2 extends Smartphone {
+let arrUser = [];
+let User1 = new User(0.2, 0, 0, 0, 0, 0);
+let User2 = new User(5, 0, 0, 0, 0, 0);
+let User3 = new User(10, 0, 0, 0, 0, 0);
+console.log(arrUser);
+arrUser.push(User1, User2, User3);
+let chargeUser1 = document.getElementById('charge1');
+chargeUser1.addEventListener('click', function () {
+    User1.chargeCredit();
+});
+let chargeUser2 = document.getElementById('charge2');
+chargeUser2.addEventListener('click', function () {
+    User2.chargeCredit();
+});
+let chargeUser3 = document.getElementById('charge3');
+chargeUser3.addEventListener('click', function () {
+    User3.chargeCredit();
+});
+const CRONOS = document.querySelector('.call');
+let timerUser1;
+let timerUser2;
+let timerUser3;
+let intervalUser1;
+let intervalUser2;
+let intervalUser3;
+let creditUser1 = document.querySelector('.credit');
+let creditUser2 = document.querySelector('.credit');
+let creditUser3 = document.querySelector('.credit');
+let cancel = document.querySelector('.erase');
+let seeCredit = document.querySelector('#credit');
+CRONOS.addEventListener('click', function () {
+    if (User1.charge < COSTO) {
+        alert('Siamo spiacenti, ma il tuo credito è insufficiente. Effettua una ricarica');
+    }
+    else {
+        timerUser1 = setInterval(function () { User1.smartphoneCall(); }, 1000);
+        intervalUser1 = setInterval(Timer, 1000);
+    }
+});
+function Timer() {
+    let tikTok1 = document.getElementById('timer1');
+    tikTok1.innerHTML = +(User1.minutes > 9 ? User1.minutes : '0' + User1.minutes) + ':'
+        + (User1.seconds > 9 ? User1.seconds : '0' + User1.seconds);
 }
-class User3 extends Smartphone {
-}
+cancel.addEventListener('click', function () {
+    User1.eraseCalls();
+});
--- ts/script.ts
+++ ts/script.ts
@@ -1,21 +1,128 @@
-class Smartphone {
-    private _carica: number;
-    private _numeroChiamate: number; 
+const COSTO = 0.20 
 
-    constructor(carica: number, numeroChiamate: number){
-       this._carica = carica;
-       this._numeroChiamate = numeroChiamate;
+class Smartphone {
+    charge: number;
+    numCalls: number; 
+    hours: number = 0;
+    minutes: number = 0;
+    seconds: number = 0;
+    interval: number;
+    
+
+
+    constructor(charge: number, numCalls: number, hours: number, minutes: number, seconds: number, interval: number){
+       this.charge = charge;
+       this.numCalls = numCalls;
+       this.hours = hours;
+       this.minutes = minutes;
+       this.seconds = seconds;
+       this.interval = interval;
     }
 
     
 }
 
+class User extends Smartphone {
+    static charge: number;
+    constructor(charge: number, numCalls: number, hours: number, minutes: number, seconds: number, interval: number) {
+        super(charge, numCalls, hours, minutes, seconds, interval)
+    }
+    public chargeCredit(): void {
+        if(this.charge <= COSTO) {
+            this.charge = this.charge + 10
+        } else if (this.charge > COSTO){
+            this.charge = this.charge + 5
+        }
+        console.log(this.charge)
+    } 
 
-let User1 = new Smartphone(4, 2);
+    public smartphoneCall(): void {
+        this.seconds++;
+        if (this.seconds >= 60) {
+            this.seconds = 0;
+            this.minutes++;
+            this.charge = this.charge - COSTO;
+        } 
+        console.log(this.charge)
+    } 
 
-let User2 = new Smartphone(1, 1);
+    public number404(): number {
+        let n404 = this.charge - ((this.minutes + 1) * COSTO) - 1; 
+        return n404; 
+    } 
 
-let User3 = new Smartphone(2, 10); 
+    public callNumber() {
+        if(this.charge >= COSTO){
+           
+        }
+        console.log(this.numCalls)
+    }
+
+    public eraseCalls():void {
+        this.numCalls = this.numCalls * 0;
+        console.log(this.numCalls)
+    }
+
+
+}
+ 
+
+let arrUser: User[] = []
+
+let User1 = new User(0.2, 0, 0, 0, 0, 0);
+let User2 = new User(5, 0, 0, 0, 0, 0);
+let User3 = new User(10, 0, 0, 0, 0, 0); 
+console.log(arrUser)
+arrUser.push(User1, User2, User3)
+
+
+let chargeUser1 = document.getElementById('charge1');
+chargeUser1!.addEventListener('click',function (){
+    User1.chargeCredit();}
+);
+
+let chargeUser2 = document.getElementById('charge2');
+chargeUser2!.addEventListener('click',function (){
+    User2.chargeCredit();}
+);
+
+let chargeUser3 = document.getElementById('charge3');
+chargeUser3!.addEventListener('click',function (){
+    User3.chargeCredit();}
+);
+
+const CRONOS = document.querySelector('.call');
+let timerUser1: number;
+let timerUser2: number;
+let timerUser3: number;
+let intervalUser1: number;
+let intervalUser2: number;
+let intervalUser3: number;
+let creditUser1 = document.querySelector('.credit');
+let creditUser2 = document.querySelector('.credit');
+let creditUser3 = document.querySelector('.credit');
+let cancel = document.querySelector('.erase');
+let seeCredit = document.querySelector('#credit');
+
+CRONOS!.addEventListener('click',function(){ 
+    if(User1.charge < COSTO){
+        alert('Siamo spiacenti, ma il tuo credito è insufficiente. Effettua una ricarica')
+    } else {
+    timerUser1 = setInterval(function(){User1.smartphoneCall()},1000);
+    intervalUser1 = setInterval(Timer,1000);
+    }
+})
+
+function Timer(){
+    let tikTok1 = document.getElementById('timer1');
+    tikTok1!.innerHTML = + (User1.minutes > 9 ? User1.minutes : '0' + User1.minutes) + ':'
+    + (User1.seconds > 9 ? User1.seconds : '0' + User1.seconds);
+}
+cancel!.addEventListener('click',function(){
+    User1.eraseCalls();
+})
+
+
 
 
 
